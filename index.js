9a44458b4c5a39727af60da32f59bd065b51c940
Hao Wang <ge_nah@hotmail.com>
2019-07-25 15:33:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string) {
+    let array = string.split(" ");
+    let new_array = [];
+    new_array.push(this.capitalize(array.shift()));
+    let words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    array.forEach(word => {
+      if(words.includes(word)) {
+        new_array.push(word);
+      }else {
+        new_array.push(this.capitalize(word));
+      }
+    })
+    return new_array.join(' ');
+  }
+
+
 }
